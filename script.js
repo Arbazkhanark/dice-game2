40d735d84622b0b0e378ae62ac30cce12c1f0e17
Arbaz Khan <arbaazkhanark23@gmail.com>
2022-08-13 22:03:01 +0530

    Final commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict'
 
+//DICE 1 RANDOM CHANGES
 var randomNumber1=Math.floor(Math.random()*7);
 console.log(randomNumber1);
 
@@ -9,6 +10,9 @@ var randomSourceDiceImg1=`/images/dice${randomNumber1}.png`;
 var dice1=document.querySelectorAll('.img')[0];
 dice1.setAttribute("src",randomSourceDiceImg1);
 
+
+
+//DICE 2 RANDOM CHANGES
 var randomNumber2=Math.floor(Math.random()*7);
 console.log(randomNumber2);
 
@@ -18,8 +22,9 @@ dice2.setAttribute("src",randomSourceDiceImg2);
 
 
 
-var player1=document.querySelector('.img1');
-var player2=document.querySelector('.img2');
+//CODE WHERE WINNER LOGIC,DRAW.
+var player1=randomNumber1;
+var player2=randomNumber2;
 
 if(player1===player2){
     document.querySelector('h1').innerHTML="DRAW";
